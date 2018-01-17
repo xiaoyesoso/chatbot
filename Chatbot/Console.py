0e9ab0dc3235a666030c53a66688b339682943dc
charlesXu86 <charlesxu86@163.com>
2018-01-17 17:23:48 +0800

    主要实现类

---
+++ Chatbot/Console.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Console.py
+@time: 2018/1/17 17:16
+@desc: 主要实现类
+"""
+
+if __name__ == '__main__':
+    pass
