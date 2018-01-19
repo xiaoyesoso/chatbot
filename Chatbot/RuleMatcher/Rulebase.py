b6d07434e258c0124aa7b4aee502ccf58fb3c18b
charlesXu86 <charlesxu86@163.com>
2018-01-19 10:11:06 +0800

    存储和计算一些相似性规则

--- Chatbot/RuleMatcher/Rulebase.py
+++ Chatbot/RuleMatcher/Rulebase.py
@@ -9,5 +9,27 @@
 @desc: 存储和计算一些相似性规则
 """
 
-if __name__ == '__main__':
-    pass
+import os
+import json
+
+from gensim.models import word2vec
+from gensim import models
+
+class Rules(object):
+    def __init__(self, domain, rule_items, children, response, word2vec_model):
+        self.id_term = domain
+        self.terms = rule_items
+        self.model = word2vec_model
+        self.response = response
+        self.children = children
+
+    def __str__(self):
+        res = 'Domain:' + self.id_term
+        if self.has_child():
+            res += ' with Children:'
+            for child in self.children:
+                res += ' ' + str(child)
+        return res
+
+    if __name__ == '__main__':
+        pass
