c23e6c18fe5a8e6433985cab159e75af72669561
charlesXu86 <charlesxu86@163.com>
2018-02-23 21:15:26 +0800

    RNN训练工具类

--- Chatbot/example/lecture_2/Utils.py
+++ Chatbot/example/lecture_2/Utils.py
@@ -6,4 +6,20 @@
 @time: 2018/02/23 
 """
 
+import codecs
+import json
+import numpy as np
+
+class VocabularyLoader(object):
+    def load_vocab(self, vocab_file, encoding):
+        pass
+
+class BatchGenerator(object):
+    def __init__(self):
+        pass
+
+
+def batch2string(batch, index_vocab_dict):
+    return ''.join(list(map(index_vocab_dict.get, batch)))
+
 
